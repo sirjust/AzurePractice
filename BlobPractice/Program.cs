cb831a80087af81fea2c41888918336abfa53aa9
sirjust <theoperaking@yahoo.com>
2021-05-22 15:49:12 -0400

    Add container practice for web app and blob project

---
+++ BlobPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace BlobPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            BlobServiceClient client = new BlobServiceClient(configuration["ConnectionString"]);
+            // CreateContainer(client, configuration);
+            UploadBlob(client, configuration);
+            Console.ReadKey();
+        }
+
+        static void UploadBlob(BlobServiceClient client, IConfiguration config)
+        {
+            BlobContainerClient containerClient = client.GetBlobContainerClient(config["ContainerName"]);
+            BlobClient blobClient = containerClient.GetBlobClient(config["BlobName"]);
+            blobClient.Upload(config["BlobLocation"]);
+
+            Console.WriteLine("Blob has been uploaded");
+        }
+
+        static void CreateContainer(BlobServiceClient client, IConfiguration config)
+        {
+            client.CreateBlobContainer(config["ContainerName"]);
+
+            Console.WriteLine("Container created");
+        }
+    }
+}
